2a7890b28061f32ddf04cde7f8560e2b4045c1ad
heytitle <pat.chormai@gmail.com>
2017-06-22 01:03:41 +0200

    update spearmint goldstein

--- spearmint-config/goldstein_price_2d/goldstein_price_2d.py
+++ spearmint-config/goldstein_price_2d/goldstein_price_2d.py
@@ -1,9 +1,11 @@
 import imp
+import numpy as np
 
-utils = imp.load_source('utils', '../../utils.py')
+bm = imp.load_source('benchmarkfunctions', '../../benchmarkfunctions.py')
 
 def main(job_id, params):
-    y = utils.goldstein_price(params['x1'], params['x2'])
+    y = bm.goldstein_price([params['x1'][0], params['x2'][0]])
+    y = float(y)
     print ('job #%2d : (%2.5f,%2.5f)\t->\ty=%f' % ( job_id, params['x1'], params['x2'], y))
 
-    return float(y)
+    return y
