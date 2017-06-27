e6401b61c97d70fcd19c6bce1cb0ee9d714eefaf
heytitle <pat.chormai@gmail.com>
2017-06-27 23:36:04 +0200

    Update notebooks

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import time
 import pandas as pd
 import numpy as np
 import re
+import glob
 
 from os import listdir
 from os.path import isfile, join
@@ -39,3 +40,52 @@ def min_up_to_k(data):
         mins_up_to_k.append(m)
 
     return mins_up_to_k
+
+def string_to_number(st):
+    stt = re.sub('^-','',st)
+    if re.match('^\d+$', stt):
+        return int(st)
+    elif re.match('[\d\.]+',stt):
+        return float(st)
+    return st
+
+def extract_lstm_trained_output(log_dir, debug = False):
+
+    config = loadConfig()
+    path = config['BASE_LOG_DIR'] + '/' + log_dir + '/*'
+
+    files = glob.glob(path)
+    no_files = len(files)
+    if debug :
+        print('Found %d files in %s' % (no_files, path))
+
+    results = []
+    for fp in files:
+        with open(fp) as f:
+            columns = dict()
+            lines = f.readlines()
+            hyper_block = False
+            for line in lines:
+                reg_ex = '^-+'
+                if re.match(reg_ex, line):
+                    hyper_block = not hyper_block
+
+                if hyper_block and not re.match(reg_ex, line):
+                    k,v = re.split(" *: +", line.strip())
+                    columns[k] = string_to_number(v)
+
+            last_lines = lines[-2].replace("Last output: ","")
+
+            for t in last_lines.split('|'):
+                k,v = re.split(" *: +", t.strip())
+                columns[k] = string_to_number(v)
+            results.append(columns)
+    return pd.DataFrame(results)
+
+'''
+Return RNN Scope (aka model name) of trained model for particular
+dimension, kernel, loss and no. of hidden units
+'''
+def get_trained_model(dim, kernel, loss, no_hidden_unit=100):
+    df = extract_lstm_trained_output('%dd-%s' % (dim, kernel) )
+    return df.loc[ (df['Number of hidden Units'] == no_hidden_unit)  & (df['Loss'] == loss) ]['RNN Scope'].values[0]
